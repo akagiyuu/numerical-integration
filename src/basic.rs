83bb8e9a79a8b60326e9393b5aaf6da6450a34c9
AkagiYuu <khanghuynh0245@gmail.com>
2024-08-20 22:33:43 +0700

    init

---
+++ src/basic.rs
@@ -0,0 +1,53 @@
+use rayon::iter::{ParallelBridge, ParallelIterator};
+
+use crate::DEFAULT_PARTITION_COUNT;
+
+#[allow(clippy::redundant_closure)]
+fn _left_end_point(a: f64, b: f64, f: impl Fn(f64) -> f64 + Sync) -> impl Fn(usize) -> f64 {
+    move |n| {
+        assert!(n > 0);
+
+        if a == b {
+            return 0.;
+        }
+
+        let step = (b - a) / n as f64;
+        (0..n)
+            .par_bridge()
+            .map(|i| a + i as f64 * step)
+            .map(|x_i| f(x_i))
+            .sum::<f64>()
+            * step
+    }
+}
+
+pub fn left_end_point(a: f64, b: f64, f: impl Fn(f64) -> f64 + Sync) -> f64 {
+    let approximation_f = _left_end_point(a, b, f);
+
+    approximation_f(DEFAULT_PARTITION_COUNT)
+}
+
+#[allow(clippy::redundant_closure)]
+fn _right_end_point(a: f64, b: f64, f: impl Fn(f64) -> f64 + Sync) -> impl Fn(usize) -> f64 {
+    move |n| {
+        assert!(n > 0);
+
+        if a == b {
+            return 0.;
+        }
+
+        let step = (b - a) / n as f64;
+        (1..=n)
+            .par_bridge()
+            .map(|i| a + i as f64 * step)
+            .map(|x_i| f(x_i))
+            .sum::<f64>()
+            * step
+    }
+}
+
+pub fn right_end_point(a: f64, b: f64, f: impl Fn(f64) -> f64 + Sync) -> f64 {
+    let approximation_f = _left_end_point(a, b, f);
+
+    approximation_f(DEFAULT_PARTITION_COUNT)
+}
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+mod basic;
+
+const DEFAULT_PARTITION_COUNT: usize = 10000000;
+
+pub use basic::*;
